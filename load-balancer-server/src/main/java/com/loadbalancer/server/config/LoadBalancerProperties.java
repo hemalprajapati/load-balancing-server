56ced5b9c730d8de1d98fa647b1c581198f385aa
Hemal.Prajapati <=>
2022-09-11 22:02:03 +0530

    Removing mode option

--- load-balancer-server/src/main/java/com/loadbalancer/server/config/LoadBalancerProperties.java
+++ load-balancer-server/src/main/java/com/loadbalancer/server/config/LoadBalancerProperties.java
@@ -7,30 +7,15 @@ import java.util.List;
 @ConfigurationProperties(prefix = "loadbalancer")
 public class LoadBalancerProperties {
 
-    /**
-     * Application mode: see {@link Mode}.
-     */
-    private Mode mode;
-
+    
     /**
      * Load Balancer endpoints: see {@link Endpoint}.
      */
     private List<Endpoint> endpoints;
 
-    /*** Getters ***/
-
-    public Mode getMode() {
-        return mode;
-    }
 
     public List<Endpoint> getEndpoints() {
         return endpoints;
-    }
-
-    /*** Setters ***/
-
-    public void setMode(Mode mode) {
-        this.mode = mode;
     }
 
     public void setEndpoints(List<Endpoint> endpoints) {
